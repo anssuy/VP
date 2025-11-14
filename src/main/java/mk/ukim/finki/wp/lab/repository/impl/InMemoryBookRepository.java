d888e1ea3f2ea20083f5d46412d3a7f3e8707d14
anssuy <anacaik01@gmail.com>
2025-11-14 12:23:46 +0100

    Lab 2

--- src/main/java/mk/ukim/finki/wp/lab/repository/impl/InMemoryBookRepository.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/impl/InMemoryBookRepository.java
@@ -6,9 +6,11 @@ import mk.ukim.finki.wp.lab.repository.BookRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class InMemoryBookRepository implements BookRepository {
+
     @Override
     public List<Book> findAll() {
         return DataHolder.books;
@@ -17,6 +19,26 @@ public class InMemoryBookRepository implements BookRepository {
     @Override
     public List<Book> searchBooks(String text, Double rating) {
         return DataHolder.books.stream()
-                .filter(book -> book.getTitle().contains(text) && book.getAverageRating() >= rating).toList();
+                .filter(b -> b.getTitle().contains(text) && b.getAverageRating() >= rating)
+                .toList();
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return DataHolder.books.stream()
+                .filter(book -> book.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Book save(Book book) {
+        DataHolder.books.removeIf(m -> m.getId().equals(book.getId()));
+        DataHolder.books.add(book);
+        return book;
+    }
+
+    @Override
+    public void delete(Long id) {
+        DataHolder.books.removeIf(book -> book.getId().equals(id));
     }
 }
